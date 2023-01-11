f02b0204e7bb04c0d93fb765ccbb4f5f3a0685b4
Airadarey <airadarey@gmail.com>
2023-01-11 10:33:20 +0600

    upd main.js

--- main.js
+++ main.js
@@ -11,8 +11,23 @@ for (let index = 0; index < numberOfFilms; index++) {
     const oneOfMovies = prompt('Один из последних просмотренных фильмов?', '') 
     const filmRate = prompt('На сколько оцените его?', '')
     const genre = prompt('А жанр?')
-    personalMovieDB.movies[oneOfMovies] = filmRate
-    personalMovieDB.genres.push(genre)
+    if (oneOfMovies != null && filmRate != null && genre != null && oneOfMovies != '' && filmRate != '' && genre !='' && oneOfMovies.length < 50) {
+        personalMovieDB.movies[oneOfMovies] = filmRate
+        personalMovieDB.genres.push(genre)
+        console.log('done');
+    } else {
+        console.log('error');
+        index--;
+    }
+}
+if (personalMovieDB.count < 10) {
+    alert("Просмотрено довольно мало фильмов")
+} else if (10 <= personalMovieDB.count <= 30) {
+    alert("Вы классический зритель")
+} else if (personalMovieDB.count > 30) {
+    alert("Вы киноман")
+} else {
+    alert("Произошла ошибка")
 }
 
 console.log(personalMovieDB);
